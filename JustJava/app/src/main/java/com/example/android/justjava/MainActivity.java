8430d418fc01f50f7f96a6fde6dbee80939ae300
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2017-07-23 23:22:02 +0530

    Finished Just Java ;)

--- JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -3,26 +3,29 @@ package com.example.android.justjava;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.animation.Interpolator;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int quantity = 1;
+    private int price = 5;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        displayPrice(quantity * price);
+        display(quantity);
     }
 
     /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
-        int quantity = Integer.parseInt(quantityTextView.getText().toString());
-        displayPrice(quantity * 5);
+        displayPrice(quantity * price);
     }
 
     /**
@@ -34,14 +37,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void increment(View view) {
-        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
-        quantityTextView.setText(Integer.parseInt(quantityTextView.getText().toString()) + 1 + "");
+        display(++quantity);
     }
 
     public void decrement(View view) {
-        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
-        if (!quantityTextView.getText().equals("0"))
-            quantityTextView.setText(Integer.parseInt(quantityTextView.getText().toString()) - 1 + "");
+        if (quantity > 1) {
+            display(--quantity);
+        } else
+            Toast.makeText(this, "Oops, quantity can't be less than one", Toast.LENGTH_SHORT).show();
     }
 
     /**
