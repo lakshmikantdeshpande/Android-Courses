aba8a5edd1fdad86f80c3b4269f7e6383dc86d7b
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2017-07-25 22:15:41 +0530

    Just Java finished

--- JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -25,7 +25,10 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        displayPrice(quantity * price);
+        if (quantity >= 10)
+            displayRichMessage();
+        else
+            displayPrice(quantity * price);
     }
 
     /**
@@ -52,6 +55,11 @@ public class MainActivity extends AppCompatActivity {
      */
     private void displayPrice(int number) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number) + "\nThank you!");
+    }
+
+    private void displayRichMessage() {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText("You sound rich :P");
     }
 }
