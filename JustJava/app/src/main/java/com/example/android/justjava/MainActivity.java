7c2ea8c49fa9bcf89bcb7b298a92a2088c966f29
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2017-07-27 00:47:21 +0530

    Intents

--- JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.android.justjava;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final String TAG = MainActivity.class.getName();
     private int quantity = 1;
     private int price = 5;
 
@@ -15,18 +20,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int price = calculatePrice();
-        String priceMessage = createOrderSummary(price);
-        displayMessage(priceMessage);
     }
 
     /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int price = calculatePrice();
-        String priceMessage = createOrderSummary(price);
-        displayMessage(priceMessage);
+        EditText editText = (EditText) findViewById(R.id.name);
+        String name = editText.getText().toString();
+        if (name.equals("")) {
+            Toast.makeText(this, "Oops, you forgot to put a name in !", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        CheckBox checkBox = (CheckBox) findViewById(R.id.whipped_cream);
+        boolean whippedCream = checkBox.isChecked();
+        checkBox = (CheckBox) findViewById(R.id.chocolate);
+        boolean chocolate = checkBox.isChecked();
+
+        int price = calculatePrice(whippedCream, chocolate);
+        String orderSummary = createOrderSummary(name, price, whippedCream, chocolate);
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:"));
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Just Java order for " + name);
+        intent.putExtra(Intent.EXTRA_TEXT, orderSummary);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 
     /**
@@ -38,19 +59,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void increment(View view) {
-        displayQuantity(++quantity);
+        if (quantity < 100) {
+            displayQuantity(++quantity);
+        }
     }
 
     public void decrement(View view) {
         if (quantity > 1) {
             displayQuantity(--quantity);
-        } else
-            Toast.makeText(this, "Oops, quantity can't be less than one", Toast.LENGTH_SHORT).show();
-    }
-
-    private void displayMessage(String message) {
-        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
-        orderSummaryTextView.setText(message);
+        }
     }
 
     /**
@@ -58,16 +75,18 @@ public class MainActivity extends AppCompatActivity {
      *
      * @Param numberOfCoffees
      */
-    private int calculatePrice() {
-        return quantity * 5;
+    private int calculatePrice(boolean whippedCream, boolean chocolate) {
+        return quantity * (price + (whippedCream ? 1 : 0) + (chocolate ? 2 : 0));
     }
 
     /*
      * Create the order summary
      */
-    private String createOrderSummary(int price) {
+    private String createOrderSummary(String name, int price, boolean whippedCream, boolean chocolate) {
         StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("Name = Kaptain Kunal\n");
+        stringBuilder.append("Name = " + name + "\n");
+        stringBuilder.append("Add whipped cream? " + whippedCream + "\n");
+        stringBuilder.append("Add chocolate? " + chocolate + "\n");
         stringBuilder.append("Quantity = " + quantity + "\n");
         stringBuilder.append("Total = $" + price + "\n");
         stringBuilder.append("Thank you!");
