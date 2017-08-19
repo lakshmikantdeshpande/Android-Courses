59251ce122a2ddf7ee40e281ef69a57cdf4e9aac
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2017-08-19 13:26:56 +0530

    Dynamic intent filter to prevent BR from listening when app is not running

--- Tutorial/11_Broadcast_receiver_demo/app/src/main/java/com/example/android/a11_broadcast_receiver_demo/MainActivity.java
+++ Tutorial/11_Broadcast_receiver_demo/app/src/main/java/com/example/android/a11_broadcast_receiver_demo/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.android.a11_broadcast_receiver_demo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.BroadcastReceiver;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BroadcastReceiver broadcastReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        broadcastReceiver = new MyBroadcastReceiver();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        IntentFilter intentFilter = new IntentFilter("android.provider.Telephony.SMS_RECEIVED");
+        registerReceiver(broadcastReceiver, intentFilter);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(broadcastReceiver);
     }
 }
