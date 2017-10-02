e7617afcb07d5984a08dffbc2dd4f06326db6c2d
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2017-10-02 15:59:16 +0530

    Settings screen added to the Sunshine App

---
+++ ud851- Exercises/S06.02-Exercise-SettingsFragment/app/src/main/java/com/example/android/sunshine/SettingsFragment.java
@@ -0,0 +1,85 @@
+package com.example.android.sunshine;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.preference.CheckBoxPreference;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceFragmentCompat;
+import android.support.v7.preference.PreferenceScreen;
+
+public class SettingsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+
+    // TODO (4) Create SettingsFragment and extend PreferenceFragmentCompat
+
+    // Do steps 5 - 11 within SettingsFragment
+    // TODO (10) Implement OnSharedPreferenceChangeListener from SettingsFragment
+
+    // TODO (8) Create a method called setPreferenceSummary that accepts a Preference and an Object and sets the summary of the preference
+
+    // TODO (5) Override onCreatePreferences and add the preference xml file using addPreferencesFromResource
+
+
+    // TODO (13) Unregister SettingsFragment (this) as a SharedPreferenceChangedListener in onStop
+
+    // TODO (12) Register SettingsFragment (this) as a SharedPreferenceChangedListener in onStart
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String s) {
+ /* Add 'general' preferences, defined in the XML file */
+        addPreferencesFromResource(R.xml.pref_sunshine);
+        // Do step 9 within onCreatePreference
+        // TODO (9) Set the preference summary on each preference that isn't a CheckBoxPreference
+
+        SharedPreferences sharedPreferences = getPreferenceScreen().getSharedPreferences();
+        PreferenceScreen prefScreen = getPreferenceScreen();
+        int count = prefScreen.getPreferenceCount();
+        for (int i = 0; i < count; i++) {
+            Preference p = prefScreen.getPreference(i);
+            if (!(p instanceof CheckBoxPreference)) {
+                String value = sharedPreferences.getString(p.getKey(), "");
+                setPreferenceSummary(p, value);
+            }
+        }
+    }
+
+    private void setPreferenceSummary(Preference preference, Object value) {
+        String stringValue = value.toString();
+        String key = preference.getKey();
+
+        if (preference instanceof ListPreference) {
+            ListPreference listPreference = (ListPreference) preference;
+            int preferenceIndex = listPreference.findIndexOfValue(stringValue);
+            if (preferenceIndex >= 0) {
+                preference.setSummary(listPreference.getEntries()[preferenceIndex]);
+            }
+        } else {
+            preference.setSummary(stringValue);
+        }
+    }
+
+    // TODO (11) Override onSharedPreferenceChanged to update non CheckBoxPreferences when they are changed
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        Preference preference = findPreference(key);
+        if (null != preference) {
+            if (!(preference instanceof CheckBoxPreference)) {
+                setPreferenceSummary(preference, sharedPreferences.getString(key, ""));
+            }
+        }
+    }
+}
